8a665577719953b14b02bd7053ba0a1ca1bd5628
ATHULKNAIR <athul2639@gmail.com>
2022-02-17 22:28:37 +0530

    sample server

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express()
 
-const PORT = 3000
+const PORT =process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
     res.send('Hello World')
 })
 
-app.listen(PORT || 8000,()=>{
+app.listen(PORT ,()=>{
     console.log('listening to port 3000...')
 })
